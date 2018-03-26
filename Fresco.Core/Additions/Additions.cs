1b5f6072a1e12e6b5db9f726ab90b3e3007a3ad6
Jake Louw <dynogic@gmail.com>
2018-03-26 15:25:18 -0400

    Completed binding for Fresco.Core

---
+++ Fresco.Core/Additions/Additions.cs
@@ -0,0 +1,37 @@
+﻿namespace Com.Facebook.Datasource
+{
+    partial class FirstAvailableDataSourceSupplier
+    {
+        Java.Lang.Object Com.Facebook.Common.Internal.ISupplier.Get()
+        {
+            return Android.Runtime.Extensions.JavaCast<Java.Lang.Object>(Get());
+        }
+    }
+
+    partial class IncreasingQualityDataSourceSupplier
+    {
+        Java.Lang.Object Com.Facebook.Common.Internal.ISupplier.Get()
+        {
+            return Android.Runtime.Extensions.JavaCast<Java.Lang.Object>(Get());
+        }
+    }
+
+    partial class RetainingDataSourceSupplier
+    {
+        Java.Lang.Object Com.Facebook.Common.Internal.ISupplier.Get()
+        {
+            return Android.Runtime.Extensions.JavaCast<Java.Lang.Object>(Get());
+        }
+    }
+}
+
+namespace Com.Facebook.Common.Executors
+{
+    partial class ScheduledFutureImpl
+    {
+        int Java.Lang.IComparable.CompareTo(Java.Lang.Object obj)
+        {
+            return CompareTo(Android.Runtime.Extensions.JavaCast<Java.Util.Concurrent.IDelayed>(obj));
+        }
+    }
+}
