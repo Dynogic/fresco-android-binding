c1235866a908529ee29c681c0fdc82b1d8a3af75
bill <bill@disa.im>
2018-04-03 15:48:45 -0400

    Adding in Fresco.ImagePipeline.OkHttp.

---
+++ Fresco.ImagePipeline.OkHttp/Additions/Additions.cs
@@ -0,0 +1,18 @@
+﻿using Android.Content;
+using Com.Facebook.Imagepipeline.Core;
+using Com.Squareup.Okhttp;
+
+namespace Com.Facebook.Imagepipeline.Backends.Okhttp
+{
+    public partial class OkHttpImagePipelineConfigFactory
+    {
+        // TODO: Expose API in bindings for commented out sections
+        public static /* ImagePipelineConfig.Builder*/ void NewBuilder(Context context, OkHttpClient okHttpClient)
+        {
+            /*
+            return ImagePipelineConfig.NewBuilder(context)
+                .SetNetworkFetcher(new OkHttpNetworkFetcher(okHttpClient));
+            */
+        }
+    }
+}
